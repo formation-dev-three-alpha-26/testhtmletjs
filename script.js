ca2e1e2c1a1893f3b6ea7712eed14ffdb511f65b
imen-mekni <113175332+imen-mekni@users.noreply.github.com>
2026-03-02 20:59:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("hi");
+
+
+var text = document.getElementsByTagName("h1")
+
+console.log(text[0].innerHTML , "text");
+
+
+var image = document.getElementById("x")
+console.log(image );
